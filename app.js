e65bb9a46dd807a4bca971bd5cbbbda7d1cb63c2
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-04-05 16:04:54 +0900

    #2.0 Canvas Events

---
+++ app.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+function onMouseUp(event) {
+  stopPainting();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
